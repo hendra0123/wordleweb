00aba990197d0366ceda12b60a1fb417997d7b58
hendra0123 <nerstuff24@gmail.com>
2023-05-31 00:28:01 +0800

    commit100

---
+++ Code/addscript.js
@@ -0,0 +1,7 @@
+function openForm1() {
+    document.getElementById("wrab").style.display = "block";
+}
+
+function closeForm1() {
+    document.getElementById("wrab").style.display = "none";
+}
---
+++ Code/guestscript.js
@@ -0,0 +1,15 @@
+function openForm1() {
+    document.getElementById("wrab").style.display = "block";
+}
+
+function closeForm1() {
+    document.getElementById("wrab").style.display = "none";
+}
+
+function openForm2() {
+    document.getElementById("wrab2").style.display = "block";
+}
+
+function closeForm2() {
+    document.getElementById("wrab2").style.display = "none";
+}
